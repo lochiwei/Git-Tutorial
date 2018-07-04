5cec866e0c700bbf0b002ef442affcecf00d6972
lochiwei <lochiwei.ios.dev.free@gmail.com>
2018-07-04 16:31:40 +0800

    added hello message to git.

--- Git Tutorial/ViewController.swift
+++ Git Tutorial/ViewController.swift
@@ -9,17 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let msg = "Hello, Git!"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print(msg)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
